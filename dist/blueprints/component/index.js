347ffb094373fe926449f813c86cf301012ebdbf
Adrià Fontcuberta <adria@calidae.com>
2018-06-12 12:58:37 +0200

    Remove dist folder from github

--- dist/blueprints/component/index.js
+++
@@ -1,7 +0,0 @@
-{% if container %}import {{name}}Container from './{{name}}Container'
-
-export default {{name}}Container
-{% else %}import {{name}} from './{{name}}'
-
-export default {{name}}
-{% endif %}
